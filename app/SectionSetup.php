05eab7c13598a17411470e9b25140fde738be646
moinularif <arif.cuet.08@gmail.com>
2022-02-07 12:25:46 +0600

    Remove Extra Code from Machine Controller

--- app/SectionSetup.php
+++ app/SectionSetup.php
@@ -3,8 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class SectionSetup extends Model
 {
-    //
+   public static function getSectionSetupsForSelect(){
+       return DB::table('section_setups')
+                ->select('id', 'name')
+                ->where('status', '=', 'A')
+                ->orderBy('name', 'ASC')
+                ->get();
+   }
 }
